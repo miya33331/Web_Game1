7684d7fbf218ee0029f56fb59e034e7b4ed3b766
miya33331 <135804436+miya33331@users.noreply.github.com>
2024-02-14 18:44:39 +0900

    各アイテムのオブジェクトを追加

--- move.js
+++ move.js
@@ -28,3 +28,41 @@ document.body.addEventListener('keydown', (event) => {
         moveLeft();
     }
 });
+
+const position = {
+    // スクリーン
+    screen : {width : 0, height : 0},
+    // ブロック
+    block : [{x : 0, y : 0, width : 0, height : 0}, {x : 0, y : 0, width : 0, height : 0}, {x : 0, y : 0, width : 0, height : 0}, {x : 0, y : 0, width : 0, height : 0}],
+    // プレイヤー
+    player : {x : 0, y : 0, width : 0, height : 0},
+    // 敵
+    enemy : {x : 0, y : 0, width : 0, height : 0},
+    // 攻撃判定
+    attack : {x : -9999, y : -9999, width : -9999, height : -9999},
+    // 重なり判定
+    overlap : (obj1, obj2) => {
+        for (let i = 0; i < 2; i++) {
+            if (obj1.x <= obj2.x && obj2.x <= obj1.x + obj1.width) {
+                if (obj1.y <= obj2.y && obj2.y <= obj1.y + obj1.height){
+                    return true;
+                }
+                if (obj1.y <= obj2.y + obj2.height && obj2.y + obj2.height <= obj1.y + obj1.height){
+                    return true;
+                }
+            }
+            if (obj1.x <= obj2.x + obj2.width && obj2.x + obj2.width <= obj1.x + obj1.width) {
+                if (obj1.y <= obj2.y && obj2.y <= obj1.y + obj1.height){
+                    return true;
+                }
+                if (obj1.y <= obj2.y + obj2.height && obj2.y + obj2.height <= obj1.y + obj1.height){
+                    return true;
+                }
+            }
+            const tmp = obj1;
+            obj1 = obj2;
+            obj2 = tmp;
+        }
+        return false;
+    }
+};
